992352fb2b5faf123ef9a97161678cfe3bf805a1
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-02 13:44:55 +0530

    Modified Repo

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
-function lovely(){
-    var x = document.getElementById("xyz");
-    if(x.style.display=="none")
-    x.style.display="block"
-    else
-    x.style.display="none"
-  }
+function lovely() {
+  var x = document.getElementById("xyz");
+  if (x.style.display == "none")
+    x.style.display = "block"
+  else
+    x.style.display = "none"
+}
